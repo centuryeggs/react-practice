943b6a14826d1c4ae04bf74b61f9cac11eb2536c
centuryeggs <junjianch@gmail.com>
2024-01-16 23:15:45 +0800

    feat: rearrange

--- src/Comments.tsx
+++ src/Comments.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Loading } from './Loading.tsx'
 import axios from 'axios'
+import dayjs from 'dayjs'
 
 type Comment = {
   createdTime: string,
@@ -8,6 +9,7 @@ type Comment = {
   userId: string,
   username: string,
   profilePicture: string,
+  likeCount: number,
   id: string
 }
 // const userInfo = {
@@ -19,6 +21,25 @@ function Comments() {
   const [list, setList] = useState([])
   const [count, setCount] = useState([])
   const [loading, setLoading] = useState(false)
+  const [activeTag, setActiveTag] = useState(0)
+
+  const activeCls = "text-blue-500 hover:text-blue-4"
+  const noActiveCls = "hover:text-blue-400" 
+
+  function rearrange(params: number) {
+    let newList
+    activeTag !== params && setActiveTag(params)
+    if (params === 0) {
+      newList = list.sort((a: Comment, b: Comment) => {
+        return dayjs(b.createdTime).valueOf() - dayjs(a.createdTime).valueOf()
+      })
+    } else {
+      newList = list.sort((a: Comment, b: Comment) => {
+        return b.likeCount - a.likeCount
+      })
+    }
+    setList([...newList])
+  }
   useEffect(() => {
     setLoading(true)
     axios.get('http://localhost:3000/comments').then(res => {
@@ -34,11 +55,15 @@ function Comments() {
     <div className="border border-slate-50 rounded-lg p-5">
       <div className="flex items-center">
         <div className="text-3xl font-bold mr-10">{count} Comments</div>
-        <div><span>Hotest</span> | <span>Newest</span></div>
+        <div>
+          <button className={!activeTag ? activeCls : noActiveCls} onClick={() => rearrange(0)}>Hotest</button>
+          <span className="mx-2">|</span>
+          <button className={activeTag ? activeCls : noActiveCls} onClick={() => rearrange(1)}>Newest</button>
+        </div>
       </div>
       <div></div>
       <ul>
-        {list.map((i:Comment) => <li key={i.id}>{i.content}</li>)}
+        {list.map((i:Comment) => <li key={i.id}>{i.content} | {i.createdTime} | {i.likeCount}</li>)}
       </ul>
       </div>
   )
