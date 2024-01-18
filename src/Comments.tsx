aaf1eec30e13f27effe7dd9d1bf8070b1a5091f3
centuryeggs <junjianch@gmail.com>
2024-01-18 23:12:41 +0800

    add comment input

--- src/Comments.tsx
+++ src/Comments.tsx
@@ -10,21 +10,23 @@ type Comment = {
   username: string,
   profilePicture: string,
   likeCount: number,
-  id: string
+  id?: string
 }
-// const userInfo = {
-//   username: '东郭皮蛋',
-//   profilePicture: 'https://i0.hdslb.com/bfs/face/d83bb1a6015f31933fb9104141243ce9e6ad929d.jpg@240w_240h_1c_1s_!web-avatar-space-header.avif'
-// }
+const userInfo = {
+  userId: '56',
+  username: 'Kristen Ramos',
+  profilePicture: 'https://i.pravatar.cc/150?img=56'
+}
 
 function Comments() {
-  const [list, setList] = useState([])
-  const [count, setCount] = useState([])
+  const [list, setList] = useState<Comment[]>([])
+  const [count, setCount] = useState(0)
   const [loading, setLoading] = useState(false)
   const [activeTag, setActiveTag] = useState(0)
+  const [newComment, setNewComment] = useState('')
 
-  const activeCls = "text-blue-500 hover:text-blue-4"
-  const noActiveCls = "hover:text-blue-400" 
+  const activeCls = "text-blue-500 hover:text-blue-600"
+  const noActiveCls = "hover:text-blue-600"
 
   function rearrange(params: number) {
     let newList
@@ -40,6 +42,11 @@ function Comments() {
     }
     setList([...newList])
   }
+  function doComment () {
+    console.log(newComment)
+    setList([...list, {...userInfo, content: newComment, createdTime: dayjs().format('YYYY-MM-DD HH:mm:ss'), likeCount: 0}])
+    setNewComment('')
+  }
   useEffect(() => {
     setLoading(true)
     axios.get('http://localhost:3000/comments').then(res => {
@@ -53,15 +60,23 @@ function Comments() {
   return (
     loading ? <Loading /> :
     <div className="border border-slate-50 rounded-lg p-5">
-      <div className="flex items-center">
-        <div className="text-3xl font-bold mr-10">{count} Comments</div>
+      <div className="flex items-end">
+        <div className="text-2xl font-bold mr-10">{count} Comments</div>
         <div>
           <button className={!activeTag ? activeCls : noActiveCls} onClick={() => rearrange(0)}>Hotest</button>
           <span className="mx-2">|</span>
           <button className={activeTag ? activeCls : noActiveCls} onClick={() => rearrange(1)}>Newest</button>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center my-4">
+        <img className="shrink-0 h-12 w-12 rounded-full mr-6" src={userInfo.profilePicture} />
+        <input value={newComment} onChange={(e) => setNewComment(e.target.value)}
+          className="block w-full h-500 px-3 py-2 mr-6 bg-neutral-200 border border-slate-900 rounded-md text-md shadow-sm text-slate-900 placeholder-slate-500
+          focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"/>
+        <button onClick={doComment} className="w-auto py-2 px-3 bg-blue-500 hover:bg-blue-600 text-md font-semibold rounded-md shadow focus:outline-none cursor-pointer">
+          Comment
+        </button>
+      </div>
       <ul>
         {list.map((i:Comment) => <li key={i.id}>{i.content} | {i.createdTime} | {i.likeCount}</li>)}
       </ul>
