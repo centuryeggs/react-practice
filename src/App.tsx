68a9249aaaae350b3c77583c982902fa25fe8529
centuryeggs <junjianch@gmail.com>
2024-01-20 18:02:35 +0800

    complete Comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Comments } from './Comments.tsx'
 
 function App() {
   return (
-    <div className="bg-neutral-800 text-slate-200 w-screen h-screen p-5">
+    <div className="bg-neutral-800 text-neutral-200 w-full h-full p-5">
       <Comments />
     </div>
   )
--- src/Comments.tsx
+++ src/Comments.tsx
@@ -20,7 +20,6 @@ const userInfo = {
 
 function Comments() {
   const [list, setList] = useState<Comment[]>([])
-  const [count, setCount] = useState(0)
   const [loading, setLoading] = useState(false)
   const [activeTag, setActiveTag] = useState(0)
   const [newComment, setNewComment] = useState('')
@@ -31,9 +30,9 @@ function Comments() {
   function rearrange(params: number) {
     let newList
     activeTag !== params && setActiveTag(params)
-    if (params === 0) {
+    if (params === 1) {
       newList = list.sort((a: Comment, b: Comment) => {
-        return dayjs(b.createdTime).valueOf() - dayjs(a.createdTime).valueOf()
+        return dayjs(a.createdTime).valueOf() - dayjs(b.createdTime).valueOf()
       })
     } else {
       newList = list.sort((a: Comment, b: Comment) => {
@@ -43,42 +42,52 @@ function Comments() {
     setList([...newList])
   }
   function doComment () {
-    console.log(newComment)
-    setList([...list, {...userInfo, content: newComment, createdTime: dayjs().format('YYYY-MM-DD HH:mm:ss'), likeCount: 0}])
+    setList([...list, {...userInfo, content: newComment, createdTime: dayjs().format('MM-DD HH:mm'), likeCount: 0}])
     setNewComment('')
   }
   useEffect(() => {
-    setLoading(true)
-    axios.get('http://localhost:3000/comments').then(res => {
+    async function getComments() {
+      setLoading(true)
+      const res = await axios.get('http://localhost:3000/comments')
       setList(res.data.list)
-      setCount(res.data.count)
       setLoading(false)
-    }).catch(err => {
-      throw new Error(err)
-    })
+    }
+    getComments()
   }, [])
   return (
     loading ? <Loading /> :
-    <div className="border border-slate-50 rounded-lg p-5">
+    <div className="border border-neutral-50 rounded-lg py-6 px-10">
       <div className="flex items-end">
-        <div className="text-2xl font-bold mr-10">{count} Comments</div>
+        <div className="text-2xl font-bold mr-10">{list.length} Comments</div>
         <div>
           <button className={!activeTag ? activeCls : noActiveCls} onClick={() => rearrange(0)}>Hotest</button>
           <span className="mx-2">|</span>
           <button className={activeTag ? activeCls : noActiveCls} onClick={() => rearrange(1)}>Newest</button>
         </div>
       </div>
-      <div className="flex items-center my-4">
+      <div className="flex items-center mt-4 mb-12">
         <img className="shrink-0 h-12 w-12 rounded-full mr-6" src={userInfo.profilePicture} />
         <input value={newComment} onChange={(e) => setNewComment(e.target.value)}
-          className="block w-full h-500 px-3 py-2 mr-6 bg-neutral-200 border border-slate-900 rounded-md text-md shadow-sm text-slate-900 placeholder-slate-500
+          className="block w-full h-500 px-3 py-2 mr-6 bg-neutral-200 border border-neutral-900 rounded-md text-md shadow-sm text-neutral-900 placeholder-neutral-500
           focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"/>
-        <button onClick={doComment} className="w-auto py-2 px-3 bg-blue-500 hover:bg-blue-600 text-md font-semibold rounded-md shadow focus:outline-none cursor-pointer">
+        <button onClick={() => newComment && doComment()} className="w-auto py-2 px-3 bg-blue-500 hover:bg-blue-600 text-md font-semibold rounded-md shadow focus:outline-none cursor-pointer">
           Comment
         </button>
       </div>
       <ul>
-        {list.map((i:Comment) => <li key={i.id}>{i.content} | {i.createdTime} | {i.likeCount}</li>)}
+        {list.map((i:Comment) =>
+          <li key={i.id} className="flex pt-2">
+            <img className="shrink-0 h-12 w-12 rounded-full mr-6" src={i.profilePicture} />
+            <div className="border-b border-neutral-600 pb-2 w-full">
+              <div className="text-neutral-400 font-bold">{i.username}</div>
+              <div className="text-lg my-2">{i.content}</div>
+              <div className="flex text-neutral-400">
+                <div>{i.createdTime}</div>
+                <div className="ml-5">like: {i.likeCount}</div>
+              </div>
+            </div>
+          </li>
+        )}
       </ul>
       </div>
   )
